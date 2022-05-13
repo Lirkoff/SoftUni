72e6f31fbd298251f0e4ca92e6dcd49b208308b4
Martin Lirkoff <martinlirkoff@gmail.com>
2022-05-13 13:40:27 +0300

    Balanced Parentheses update

--- Data Structures with Java - Fundamentals/Linear DS/Ex/03. Linear-Data-Structures-Exercises-Skeleton/src/main/java/Main.java
+++ Data Structures with Java - Fundamentals/Linear DS/Ex/03. Linear-Data-Structures-Exercises-Skeleton/src/main/java/Main.java
@@ -1,13 +1,5 @@
-import implementations.ReversedList;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
     }
 }
--- Data Structures with Java - Fundamentals/Linear DS/Ex/03. Linear-Data-Structures-Exercises-Skeleton/src/main/java/implementations/BalancedParentheses.java
+++ Data Structures with Java - Fundamentals/Linear DS/Ex/03. Linear-Data-Structures-Exercises-Skeleton/src/main/java/implementations/BalancedParentheses.java
@@ -2,15 +2,81 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.ArrayDeque;
+
+import java.util.Deque;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
+    private int middle;
 
     public BalancedParentheses(String parentheses) {
-        this.parentheses = parentheses;
+        this.parentheses = parentheses.trim();
     }
 
     @Override
     public Boolean solve() {
-        return null;
+        if (this.parentheses == null) {
+            throw new IllegalArgumentException("Empty input");
+        }
+
+        if (this.parentheses.length() % 2 != 0) {
+            return false;
+        }
+
+        if (elementNotAllowed()) {
+            return false;
+        }
+
+        this.middle = this.parentheses.length() / 2;
+
+
+        Deque<Character> stack = new ArrayDeque<>();
+        Deque<Character> queue = new ArrayDeque<>();
+
+
+        fillStackWithFirstHalf(stack);
+        fillQueueWithSecondHalf(queue);
+
+        return balanced(stack, queue);
+    }
+
+    private boolean balanced(Deque<Character> stack, Deque<Character> queue) {
+        for (int i = 0; i < middle; i++) {
+            char firstElement = stack.pop();
+            char secondElement = queue.poll();
+
+            if ((firstElement == '(' && secondElement != ')') ||(firstElement == '{' && secondElement != '}')
+                    || (firstElement == '[' && secondElement != ']')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean elementNotAllowed() {
+        String allowedElements = "{[()]}";
+        for (int i = 0; i < this.parentheses.length(); i++) {
+            char element = this.parentheses.charAt(i);
+
+            if (!allowedElements.contains(element + "")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void fillQueueWithSecondHalf(Deque<Character> queue) {
+        for (int i = this.middle; i < this.parentheses.length(); i++) {
+            char element = this.parentheses.charAt(i);
+            queue.offer(element);
+        }
+    }
+
+    private void fillStackWithFirstHalf(Deque<Character> stack) {
+        for (int i = 0; i < this.middle; i++) {
+            char element = this.parentheses.charAt(i);
+            stack.push(element);
+        }
     }
 }
